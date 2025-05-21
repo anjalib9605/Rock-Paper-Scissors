bed5ecf8e765bfa7b65ea3525e125cef724d62b8
anjalib9605 <139668631+anjalib9605@users.noreply.github.com>
2025-05-21 12:14:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,80 @@
-let winCount=0;
-let tieCount=0;
-let loseCount=0;
+let winCount= JSON.parse(localStorage.getItem('win-count')) || 0;
+let tieCount=JSON.parse(localStorage.getItem('tie-count')) || 0;
+let loseCount=JSON.parse(localStorage.getItem('lose-count')) || 0;
 
-      function computerMove() {
-        const randNum= Math.random();
-        let compMove= '';
+document.getElementById('win').textContent = winCount;
+document.getElementById('tie').textContent = tieCount;
+document.getElementById('lose').textContent = loseCount;
 
-        if (randNum<1/3) {
-          compMove='rock';
-        } else if (randNum<2/3){
-          compMove='paper';
-        } else{
-          compMove='scissors';
-        }
+function computerMove() {
+  const randNum= Math.random();
+  let compMove= '';
 
-        return compMove;
-      }
+  if (randNum<1/3) {
+    compMove='rock';
+  } else if (randNum<2/3){
+    compMove='paper';
+  } else{
+    compMove='scissors';
+  }
 
-      function userMove(userChoice) {
-        const compMove= computerMove();
-        
-        if (compMove===userChoice) {
-          resultType='tieType';
-          updatePoints()
-          document.getElementById('result-statement').textContent= `Computer also picked ${compMove}. It's a tie.`;
+  return compMove;
+}
 
-        } else if (
-          (userChoice==='rock' && compMove==='scissors') ||
-          (userChoice==='paper' && compMove==='rock') ||
-          (userChoice==='scissors' && compMove==='paper')
-        ) {
-          resultType='winType';
-          updatePoints()
-          document.getElementById('result-statement').textContent= `Computer picked ${compMove}. You won!`;
+function userMove(userChoice) {
+  const compMove= computerMove();
+  
+  if (compMove===userChoice) {
+    resultType='tieType';
+    updatePoints()
+    document.getElementById('result-statement').textContent= `Computer also picked ${compMove}. It's a tie.`;
 
-        } else {
-          resultType='loseType';
-          updatePoints()
-          document.getElementById('result-statement').textContent= `Computer picked ${compMove}. You lose.`;
-        }
-      }
+  } else if (
+    (userChoice==='rock' && compMove==='scissors') ||
+    (userChoice==='paper' && compMove==='rock') ||
+    (userChoice==='scissors' && compMove==='paper')
+  ) {
+    resultType='winType';
+    updatePoints()
+    document.getElementById('result-statement').textContent= `Computer picked ${compMove}. You won!`;
 
-     function updatePoints() {
-      if (resultType==='winType') {
-        winCount++;
-        document.getElementById('win').textContent=winCount;
-      } else if(resultType==='tieType'){
-        tieCount++;
-        document.getElementById('tie').textContent=tieCount;
-      } else {
-        loseCount++;
-        document.getElementById('lose').textContent=loseCount;
-      }
-     }
+  } else {
+    resultType='loseType';
+    updatePoints()
+    document.getElementById('result-statement').textContent= `Computer picked ${compMove}. You lose.`;
+  }
+}
 
-      function reset() {
-        winCount=0;
-        tieCount=0;
-        loseCount=0;
+function updatePoints() {
+if (resultType==='winType') {
+  winCount++;
+  localStorage.setItem('win-count', JSON.stringify(winCount));
+  document.getElementById('win').textContent=winCount;
 
-        document.getElementById('win').textContent= winCount;
-        document.getElementById('tie').textContent= tieCount;
-        document.getElementById('lose').textContent= loseCount;
+} else if(resultType==='tieType'){
+  tieCount++;
+  localStorage.setItem('tie-count', JSON.stringify(tieCount));
+  document.getElementById('tie').textContent=tieCount;
+  
+} else {
+  loseCount++;
+  localStorage.setItem('lose-count', JSON.stringify(loseCount));
+  document.getElementById('lose').textContent=loseCount;
+}
+}
 
-        document.getElementById('result-statement').textContent= 'Points reset';
-      }
+function reset() {
+  winCount=0;
+  tieCount=0;
+  loseCount=0;
+
+  localStorage.setItem('win-count', JSON.stringify(winCount));
+  localStorage.setItem('tie-count', JSON.stringify(tieCount));
+  localStorage.setItem('lose-count', JSON.stringify(loseCount));
+
+  document.getElementById('win').textContent= winCount;
+  document.getElementById('tie').textContent= tieCount;
+  document.getElementById('lose').textContent= loseCount;
+
+  document.getElementById('result-statement').textContent= 'Points reset';
+}
